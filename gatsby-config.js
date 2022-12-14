8153bc5466c5feeffd63985d0ae7944e91b19537
yan brock <waitmeforcrime@gmail.com>
2022-12-14 21:39:13 +0100

    first push of public info

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/More_Growth",
   siteMetadata: {
     title: `Task for More Growth`,
     description: `This is an app which was created as a test task which should show how I can to manage with Web Developer's responsibilities from More Growth`,
